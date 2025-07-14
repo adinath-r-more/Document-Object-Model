07e1a402a6759722582d534dd565798f50d59b2f
Adinath more <moreadi7777@gmail.com>
2025-07-14 18:44:56 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let h2 = document.querySelector("h2");
-console.dir(h2);
-
-h2.innerText = h2.innerText + "Adi More";
+let divs = document.querySelectorAll(".box");
+let idx = 1;
+for (div of divs){
+    div.innerText = `new unique value ${idx}`;
+    idx++;
+}
