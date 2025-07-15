5ba37c2c063eb37a88148b3c8ecd2edcc4adf388
Adinath more <moreadi7777@gmail.com>
2025-07-15 14:50:19 +0530

    dom manipulation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let divs = document.querySelectorAll(".box");
-let idx = 1;
-for (div of divs){
-    div.innerText = `new unique value ${idx}`;
-    idx++;
-}
+let newBtn = document.createElement("button");
+newBtn.innerText = "click me!";
+console.log(newBtn);
+
+let div = document.querySelector("div");
+div.after(newBtn);
